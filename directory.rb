603576598410df7d6f18aafa6483ff31d96fbdb3
Fergus Corcoran <fergus.corcoran@gmail.com>
2019-11-27 15:09:08 +0000

    iterating code

--- directory.rb
+++ directory.rb
@@ -1,18 +1,23 @@
 #firat we print the list of students
-student_count = 11
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked With of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Bartatheon",
+  "Norman Bates"
+]
+# and then print them
 puts "The students of Villains Academy"
 puts "------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+    puts student
+end
 # finally, we print the total number of students
 #it's important that print() doesn;t add new line characters
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
