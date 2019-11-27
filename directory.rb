728df69fa8e65ec96a506117d5f5d27b702b11b5
Fergus Corcoran <fergus.corcoran@gmail.com>
2019-11-27 16:14:20 +0000

    adding each_with_index() method

--- directory.rb
+++ directory.rb
@@ -23,8 +23,9 @@ def input_students
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    indexplusone = index + 1
+    puts "#{indexplusone}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
